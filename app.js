01a2df9e636fa20a90490456b499d1ec2cfbb570
Igor Marushenko <igormarushenko@gmail.com>
2022-01-15 12:33:41 +0200

    create a server file "app.js"

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const tasks = require("./routes/TasksRoute");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/tasks", tasks);
+
+mongoose.connect("mongodb://localhost:27017/Tasks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(port, () => console.log(`listening on port '${port}'`));
